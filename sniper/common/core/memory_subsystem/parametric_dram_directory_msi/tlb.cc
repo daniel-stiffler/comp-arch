e8a4e8cd52d5693a7782aec0a4451407519e82f5
Guillaume DIDIER <guillaume.didier@polytechnique.edu>
2018-04-07 17:18:37 -0400

    Fixed tlb.o
    TLB are uncompressed caches.

--- sniper/common/core/memory_subsystem/parametric_dram_directory_msi/tlb.cc
+++ sniper/common/core/memory_subsystem/parametric_dram_directory_msi/tlb.cc
@@ -7,7 +7,8 @@ namespace ParametricDramDirectoryMSI
 TLB::TLB(String name, String cfgname, core_id_t core_id, UInt32 num_entries, UInt32 associativity, TLB *next_level)
    : m_size(num_entries)
    , m_associativity(associativity)
-   , m_cache(name + "_cache", cfgname, core_id, num_entries / associativity, associativity, SIM_PAGE_SIZE, "lru", CacheBase::PR_L1_CACHE)
+   , m_cache(name + "_cache", cfgname, core_id, num_entries / associativity, associativity, false ,SIM_PAGE_SIZE, "lru", CacheBase::PR_L1_CACHE)
+     // compressed TLB are not supported
    , m_next_level(next_level)
    , m_access(0)
    , m_miss(0)
@@ -21,7 +22,7 @@ TLB::TLB(String name, String cfgname, core_id_t core_id, UInt32 num_entries, UIn
 bool
 TLB::lookup(IntPtr address, SubsecondTime now, bool allocate_on_miss)
 {
-   bool hit = m_cache.accessSingleLine(address, Cache::LOAD, NULL, 0, now, true);
+   bool hit = m_cache.accessSingleLine(address, Cache::LOAD, NULL, 0, now, true, NULL);
 
    m_access++;
 
@@ -46,14 +47,14 @@ TLB::lookup(IntPtr address, SubsecondTime now, bool allocate_on_miss)
 void
 TLB::allocate(IntPtr address, SubsecondTime now)
 {
-   bool eviction;
-   IntPtr evict_addr;
-   CacheBlockInfo evict_block_info;
-   m_cache.insertSingleLine(address, NULL, &eviction, &evict_addr, &evict_block_info, NULL, now);
+   WritebackLines evictions;
+   std::vector<IntPtr> eviction_addrs;
+   m_cache.insertSingleLine(address, NULL, now, &eviction_addrs, &evictions, NULL);
 
    // Use next level as a victim cache
-   if (eviction && m_next_level)
-      m_next_level->allocate(evict_addr, now);
+   if (!eviction_addrs.empty() && m_next_level)
+      for(auto& eviction_addr : eviction_addrs)
+      m_next_level->allocate(eviction_addr, now);
 }
 
 }
